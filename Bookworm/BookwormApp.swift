eee5a016ebf1d5fbc56f85bea46f41e6f79ab4fb
Eugene Cross <eugene.cross@gmail.com>
2023-09-01 09:37:18 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Eugene on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
